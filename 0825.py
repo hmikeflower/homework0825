9901df5083eaad0a8b138e15fdcbedc97f902f30
hmikeflowr <hmikeflowr@gmail.com>
2020-09-08 00:48:54 +0800

    V3

--- 0825.py
+++ 0825.py
@@ -4,19 +4,27 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-
-nums = sys.argv
-url = nums[1]
-count =int(nums[2])
-baseURL = url
-for a in range(count):
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text,"html.parser")   #"html.parser" 是啥
+def geturl(url_one):
+    resp = requests.get(url_one)
+    soup = BeautifulSoup(resp.text,"html.parser")   
     all_a_tag = soup.find_all('a')
+    #baseURL = url_one
+    clist = []
     for a_tag in all_a_tag:
-        href = a_tag.get("href") # 嘗試取得 <a> 裡的 href  屬性值
-        b = urljoin(baseURL, href)
-        print(b)
-    else:
-        url = href
-        baseURL = url
+        href = a_tag.get("href")
+        re_url = urljoin(url_one, href)
+        clist.append(re_url)
+
+    return clist       
+
+
+if __name__ == "__main__":
+
+    nums = sys.argv
+    url = nums[1]
+    count =int(nums[2])
+
+#clist = []    #這邊有的話，會出錯。
+    for cot in range(count):
+        url_list = geturl(url)
+    
